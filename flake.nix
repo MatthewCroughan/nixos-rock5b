3c299abbb498b12a0bfadc57ed76b5dbd1685d0a
matthewcroughan <matt@croughan.sh>
2026-04-12 03:03:44 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -7,15 +7,19 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "aarch64-linux" ];
       flake = {
-        nixosModules.rock5b-plus = ./rock5b.nix;
+        nixosModules.rock5b-plus = ./rock-5b-plus;
         nixosConfigurations.rock5b = inputs.nixpkgs.lib.nixosSystem {
           system = "aarch64-linux";
           specialArgs = {
             inherit inputs;
           };
           modules = [
-            ./rock5b.nix
-            ./repart.nix
+            ./rock-5b-plus
+            {
+              hardware.rock-5b-plus.enable = true;
+              hardware.rock-5b-plus.image.generateImage = true;
+              hardware.rock-5b-plus.image.useRepart = true;
+            }
           ];
         };
       };
---
+++ rock-5b-plus/disko-devices.nix
@@ -0,0 +1,72 @@
+{
+  disk = {
+    disk1 = {
+      imageSize = "10G";
+      device = "/dev/sdX";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "2G";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "rpool";
+            };
+          };
+        };
+      };
+    };
+  };
+  zpool = {
+    rpool = {
+      type = "zpool";
+      rootFsOptions = {
+        acltype = "posixacl";
+        compression = "zstd";
+        dnodesize = "auto";
+        normalization = "formD";
+        relatime = "on";
+        xattr = "sa";
+      };
+      options = {
+        ashift = "12";
+        autotrim = "on";
+      };
+
+      datasets = {
+        "root" = {
+          type = "zfs_fs";
+          options = {
+            mountpoint = "legacy";
+          };
+          mountpoint = "/";
+        };
+        "nix" = {
+          type = "zfs_fs";
+          options.mountpoint = "legacy";
+          mountpoint = "/nix";
+        };
+        "var" = {
+          type = "zfs_fs";
+          options.mountpoint = "legacy";
+          mountpoint = "/var";
+        };
+        "home" = {
+          type = "zfs_fs";
+          mountpoint = "/home";
+          options.mountpoint = "legacy";
+        };
+      };
+    };
+  };
+}
